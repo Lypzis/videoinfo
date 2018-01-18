4349e12085f04128d21a90b5a3950ae287a768e6
Lypzis <victorvp2008@hotmail.com>
2018-01-17 22:59:19 -0200

    Adding more features

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 
 //importing electron library
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg')
 
 //represents the overall app process running
-const { app, BrowserWindow } = electron; //app object
+const { app, BrowserWindow, ipcMain } = electron; //app object
 
 //event handler
 app.on('ready', () => {
    const mainWindow = new BrowserWindow({});
    mainWindow.loadURL(`file://${__dirname}/index.html`); 
 }); 
+
+//the event being listened from ipcRenderer, obs: event -> ' ' can have any name
+ipcMain.on('videoSubmitted', (event, path) => {
+    //will execute every time the event videoSubmitted is called
+    ffmpeg.ffprobe(path, (err, metadata) => {
+      console.log('Video duration is: ', metadata.format.duration);
+    });
+});
