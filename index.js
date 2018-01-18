577f1368ae4855469e0fbf71463941fbcf25aa1f
Lypzis <victorvp2008@hotmail.com>
2018-01-17 23:55:55 -0200

    stable

--- index.js
+++ index.js
@@ -3,21 +3,26 @@
 
 //importing electron library
 const electron = require('electron');
-const ffmpeg = require('fluent-ffmpeg')
+const ffmpeg = require('fluent-ffmpeg');
 
 //represents the overall app process running
 const { app, BrowserWindow, ipcMain } = electron; //app object
 
+//let allow variable to be used anywhere while only
+//having effect on that specific place
+let mainWindow;
+
 //event handler
 app.on('ready', () => {
-   const mainWindow = new BrowserWindow({});
-   mainWindow.loadURL(`file://${__dirname}/index.html`); 
+  mainWindow = new BrowserWindow({});
+  mainWindow.loadURL(`file://${__dirname}/index.html`); 
 }); 
 
 //the event being listened from ipcRenderer, obs: event -> ' ' can have any name
 ipcMain.on('videoSubmitted', (event, path) => {
     //will execute every time the event videoSubmitted is called
     ffmpeg.ffprobe(path, (err, metadata) => {
-      console.log('Video duration is: ', metadata.format.duration);
+      //send information back to the main window(html)
+      mainWindow.webContents.send('videoMetadata', metadata.format.duration);
     });
 });
